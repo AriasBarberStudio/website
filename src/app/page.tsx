05b09fab42bee909e639d50fa2c77ed01998fb56
dduzgun-security <deniz.onurduzgun@deliverect.com>
2024-04-07 12:32:46 -0400

    Fix links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -110,6 +110,7 @@ export default function Home() {
                       key={item.name}
                       href={item.href}
                       className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                      onClick={item.name === "Appointments" ? handleAppointmentsClick : undefined}
                     >
                       {item.name}
                     </a>
